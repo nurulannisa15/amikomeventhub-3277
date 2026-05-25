1ea67a849b0654b0e6ded474c7c3756d278200e6
nurulannisa15 <1501nurulannisa@gmail.com>
2026-05-25 13:41:57 +0700

    UTS_24.12.3277

--- database/migrations/2026_05_25_055449_create_partners_table.php
+++ database/migrations/2026_05_25_055449_create_partners_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('partners', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('logo');  // ← Harus SAMA dengan $fillable di model
+            $table->string('logo_url')->nullable();
             $table->timestamps();
         });
     }
